f267dd9cb664fcf45d2f775ca65023680f17b48a
Gustavo Tiago <gustavo.tiago.gts@outlook.com>
2022-10-05 13:58:41 -0300

    Primeiro Commit

---
+++ src/main/java/com/stonks/candidatestracker/CandidatesTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.stonks.candidatestracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandidatesTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandidatesTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stonks/candidatestracker/CandidatesTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stonks.candidatestracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidatesTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
